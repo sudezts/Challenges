3959ac280ebc83937151c8d081477f11d26ed0d5
sudezts <119412170+sudezts@users.noreply.github.com>
2022-12-13 10:29:30 +0300

    sudeoztaswriteNameinaRow

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -1,17 +1,23 @@
 // Online C compiler to run C program online
 #include <stdio.h>
-int main(){
- int i,j,k,n; 
- printf("Enter the number of the row :");
- scanf("%d",&n);
- 
- for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
-     
- }
-     printf("Akif")
- }
- 
-    
+
+int main() {
+	int i,j,k,n;
+	printf("enter the number of the row;");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++){
+		printf("sude\n");
+	
+	}
+	
+	printf("enter the number of the row;");
+	scanf("%d",&j);
+	
+	for(k=0;k<j;k++){
+		
+		printf("oztas\n");
+	}
+	
+	return 0;
 }
