13a67826d54b0a1becd300a2701171daf9023d3b
sudezts <119412170+sudezts@users.noreply.github.com>
2022-12-13 10:03:26 +0300

    sudeoztasprintyourname

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,19 @@
 // your name surname and age from user
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char x[100];
+    char z[100];
+    
+    printf("enter your name;\n");
+    scanf("%s",&x);
+    
+    printf("enter your surname;\n");
+    scanf("%s",&z);
+    
+    printf("your name and surname; %s %s",x, z);
+
+    return 0;
+}
