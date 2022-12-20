12a047be3aec634f3d248e6280cac4c2f016f289
nil <113453210+nil-32@users.noreply.github.com>
2022-12-20 00:41:19 -0800

    ispalindrome.c (#100)

--- isPalindrome.c
+++ isPalindrome.c
@@ -1,8 +1,5 @@
 
-//Bu code hatali olabilir cunku cok uzun surede calisiyor ve hata veriyor ama mantigi dogru olabilir buna bakin
-// hatayi giderin ve calistirin, hatayi gideren ilk kişinin adi yazilacak
-
-
+#include "stdbool.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
@@ -10,7 +7,7 @@
 bool isPalindrome(int x) {
     int i,j;
     int len = 0;
-    int flag = 0;
+    bool flag = 0;
     char str[100];
     sprintf(str,"%d",x);
     len = strlen(str);
@@ -20,18 +17,14 @@ bool isPalindrome(int x) {
             break;
         }
     }
-    if(flag == 1){
-        return false;
-    }
-    else{
-        return true;
+        return flag;
     }
     
-}
+
 
 int main() 
 {
     int x = 121;
-    printf("%d",isPalindrome(x));
+    printf("%d",isPalindrome(x)); //prints zero if the number is a palindrome
     return 0;
 }
